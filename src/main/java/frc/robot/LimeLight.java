c94126f82e2cb85c1d0b290e3f0948951a9d4cc5
Tanmayi Dasari <tanmayi.dasari@gmail.com>
2023-01-11 16:47:59 -0800

    fixed checkstyle errors

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -1,47 +1,75 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
+//import org.photonvision.PhotonUtils;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+* Handles al actions for the Limelight camera.
+*/
 public class LimeLight {
-    private PhotonCamera camera;
-	private static final double HUB_CAMERA_ANGLE = Math.toRadians(0); //RADIANS //30.25
-	//private static final double BALL_CAMERA_ANGLE = Math.toRadians(); //RADIANS
-	private static final double HUB_HEIGHT = 2.6; //METERS
-	private static final double APRIL_TAG_HEIGHT = 0.4699; //METERS
-	//private static final double BALL_HEIGHT = ; //METERS
-	private static final double CAMERA_HEIGHT = 0.584; //METERS
-	public static final double INVALID_RETURN = -2;
-	private double last_seen_location = -1;
 
-    public LimeLight() {
-		camera = new PhotonCamera("gloworm");
-	}
+  /**
+  * Photonvision camera object.
+  */
+  private PhotonCamera camera;
 
-	public void update() {
-		//SmartDashboard.putNumber("Distance", getAprilTagDistance());
-		SmartDashboard.updateValues();
+  /**
+  * angle of camera.
+  */
+  private static final double HUB_CAMERA_ANGLE = Math.toRadians(0);
 
-	}
+  /**
+  * height of AprilTag.
+  */
+  private static final double APRIL_TAG_HEIGHT = 0.4699; //METERS
 
-    public double getAprilTagDistance() {
-		camera.setPipelineIndex(1);
-		var result = camera.getLatestResult();
-		if (result.hasTargets()) {
-			//SmartDashboard.putNumber("angle", result.getBestTarget().getPitch());
-			//SmartDashboard.updateValues();
-			/*
-			return PhotonUtils.calculateDistanceToTargetMeters(
-								CAMERA_HEIGHT,
-								APRIL_TAG_HEIGHT,
-								HUB_CAMERA_ANGLE,
-								Math.toRadians(result.getBestTarget().getPitch()));
-			*/
-			//39.3701 inches = 1 meter
-			double visionDist = 39.3701 * (APRIL_TAG_HEIGHT - CAMERA_HEIGHT)/Math.tan(HUB_CAMERA_ANGLE+Math.toRadians(result.getBestTarget().getPitch()));
-			return 13.2*Math.exp(0.0168 * visionDist); //within 5 inches except when distance is 4.5-7.3 ft
-		}
-		return -1;
-	}
+  /**
+  * height of camera.
+  */
+  private static final double CAMERA_HEIGHT = 0.584; //METERS
+
+  /**
+  * invalid return constant.
+  */
+  public static final double INVALID_RETURN = -2;
+
+  /**
+  * converion constant: 39.3701 inches in a meter.
+  */
+  public static final double METERS_TO_INCHES = 39.3701;
+
+   /**
+   * Creates a new LimeLight and initiates a camera.
+   */
+  public LimeLight() {
+    camera = new PhotonCamera("gloworm");
+  }
+
+   /**
+   * Updates values on SmartDashboard.
+   */
+  public final void update() {
+    // SmartDashboard.putNumber("Distance", getAprilTagDistance());
+    SmartDashboard.updateValues();
+  }
+
+   /**
+   * Returns distance to AprilTags.
+   * @return distance to AprilTag
+   */
+  public final double getAprilTagDistance() {
+    camera.setPipelineIndex(1);
+    var result = camera.getLatestResult();
+    if (result.hasTargets()) {
+      double heightDiff = APRIL_TAG_HEIGHT - CAMERA_HEIGHT;
+      double pitch = result.getBestTarget().getPitch();
+      double angle = HUB_CAMERA_ANGLE + Math.toRadians(pitch);
+      double visionDist = heightDiff / Math.tan(angle);
+      double visionDistInches = METERS_TO_INCHES * visionDist;
+      return visionDist;
+    }
+    return -1;
+}
 }
