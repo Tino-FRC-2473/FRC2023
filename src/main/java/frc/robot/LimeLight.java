63d5e64b5eb067d08ff118a2e7eb141c56cb9099
allison_yu <ayu532@student.fuhsd.org>
2023-01-14 13:26:59 -0800

    Fixed checkstyle for LimeLight and Constants classes

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -7,55 +7,39 @@ import org.photonvision.PhotonUtils;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
-/**
-* Handles al actions for the Limelight camera.
-*/
+/** Handles all actions for the Limelight camera. */
 public class LimeLight {
-
-  /**
-  * Photonvision camera object.
-  */
+  /** Photonvision camera object. */
   private PhotonCamera camera;
 
-  /**
-  * angle of camera.
-  */
+  /** angle of camera (radians). */
   private static final double CAMERA_ANGLE = Math.toRadians(0);
 
-  /**
-  * height of AprilTag.
-  */
-  private static final double APRIL_TAG_HEIGHT = 0.4444; //METERS
+  /** height of AprilTag (meters). */
+  private static final double APRIL_TAG_HEIGHT = 0.4444;
 
-  /**
-  * height of camera.
-  */
-  private static final double CAMERA_HEIGHT = 0.3810; //METERS (on robot .4953, on ground .3810)
+  /** height of camera (meters). On robot is .4953, on ground is .3810. */
+  private static final double CAMERA_HEIGHT = 0.3810;
 
-  /**
-  * invalid return constant.
-  */
+  /** invalid return constant. */
   public static final double INVALID_RETURN = -2;
 
-  /**
-  * converion constant: 39.3701 inches in a meter.
-  */
+  /** converion constant: 39.3701 inches in a meter. */
   public static final double METERS_TO_INCHES = 39.3701;
 
-   /**
-   * Creates a new LimeLight and initiates a camera.
-   */
+  /** converion constant: degrees to radians. */
+  private static final double DEG_TO_RAD = Math.PI / 180;
+
+  /** Creates a new LimeLight and initiates a camera. */
   public LimeLight() {
     camera = new PhotonCamera("gloworm");
   }
 
-   /**
-   * Updates values on SmartDashboard.
-   */
+  /** Updates values on SmartDashboard. */
   public final void update() {
     SmartDashboard.putNumber("Distance", getAprilTagDistance());
     SmartDashboard.updateValues();
-	  System.out.println("Distance " + getAprilTagDistance());
+    System.out.println("Distance " + getAprilTagDistance());
   }
 
    /**
@@ -66,7 +50,7 @@ public class LimeLight {
     camera.setPipelineIndex(1);
     var result = camera.getLatestResult();
     if (result.hasTargets()) {
-      /* 
+      /*
       double heightDiff = APRIL_TAG_HEIGHT - CAMERA_HEIGHT;
       double pitch = result.getBestTarget().getPitch();
       double angle = CAMERA_ANGLE + Math.toRadians(pitch);
@@ -74,14 +58,14 @@ public class LimeLight {
       double visionDistInches = METERS_TO_INCHES * visionDist;
       return visionDistInches;
       */
-
-	   return METERS_TO_INCHES * PhotonUtils.calculateDistanceToTargetMeters(
-	 							CAMERA_HEIGHT,
-	 							APRIL_TAG_HEIGHT,
-	 							CAMERA_ANGLE,
-	 							(result.getBestTarget().getPitch()*.0174533));
-	  }
-	return -1;
+      return METERS_TO_INCHES
+        * PhotonUtils.calculateDistanceToTargetMeters(
+                CAMERA_HEIGHT,
+                APRIL_TAG_HEIGHT,
+                CAMERA_ANGLE,
+                (result.getBestTarget().getPitch() * DEG_TO_RAD));
+    }
+    return -1;
   }
 
 }
--- src/main/java/frc/robot/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/PhotonCameraWrapper.java
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -112,7 +112,7 @@
       */
      public Pair<Pose2d, Double> getEstimatedGlobalPose() {
          //robotPoseEstimator.setReferencePose(prevEstimatedRobotPose);
- 
+
          double currentTime = Timer.getFPGATimestamp();
          Optional<Pair<Pose3d, Double>> result = robotPoseEstimator.update();
          if (result.isPresent()) {
