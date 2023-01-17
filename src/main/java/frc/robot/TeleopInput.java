73240ba0693e9dd880d8d022c1545d853d9d13c8
Daiji Uchino <duchino292@student.fuhsd.org>
2023-01-16 18:54:39 -0800

    Updated button layout

--- src/main/java/frc/robot/TeleopInput.java
+++ src/main/java/frc/robot/TeleopInput.java
@@ -15,13 +15,15 @@ public class TeleopInput {
 	private static final int MECH_JOYSTICK_PORT = 0;
 	private static final int DRIVE_JOYSTICK_PORT = 1;
 
-	private static final int RELEASE_BUTTON = 3;
-	private static final int PIVOT_INCREASE_ID = 1;
-	private static final int PIVOT_DECREASE_ID = 2;
-	private static final int EXTEND_ID = 3;
-	private static final int RETRACT_ID = 4;
-	private static final int AIM_HIGH_ID = 5;
-	private static final int AIM_MID_ID = 6;
+	private static final int OPEN_BUTTON = 8;
+	private static final int CUBE_BUTTON = 10;
+	private static final int CONE_BUTTON = 12;
+	private static final int PIVOT_INCREASE_BUTTON = 5;
+	private static final int PIVOT_DECREASE_BUTTON = 3;
+	private static final int EXTEND_BUTTON = 6;
+	private static final int RETRACT_BUTTON = 4;
+	private static final int AIM_HIGH_BUTTON = 7;
+	private static final int AIM_MID_BUTTON = 9;
 	/* ======================== Private variables ======================== */
 	// Input objects
 	private Joystick mechJoystick;
@@ -63,14 +65,14 @@ public class TeleopInput {
 	 * @return True if button is pressed
 	 */
 	public boolean isPivotIncreaseButtonPressed() {
-		return mechJoystick.getRawButton(PIVOT_INCREASE_ID);
+		return mechJoystick.getRawButton(PIVOT_INCREASE_BUTTON);
 	}
 	/**
 	 * Get the value of the Pivot Decrease button.
 	 * @return True if button is pressed
 	 */
 	public boolean isPivotDecreaseButtonPressed() {
-		return mechJoystick.getRawButton(PIVOT_DECREASE_ID);
+		return mechJoystick.getRawButton(PIVOT_DECREASE_BUTTON);
 	}
 
 	/**
@@ -78,7 +80,7 @@ public class TeleopInput {
 	 * @return True if button is pressed
 	 */
 	public boolean isExtendButtonPressed() {
-		return mechJoystick.getRawButton(EXTEND_ID);
+		return mechJoystick.getRawButton(EXTEND_BUTTON);
 	}
 
 	/**
@@ -86,7 +88,7 @@ public class TeleopInput {
 	 * @return True if button is pressed
 	 */
 	public boolean isRetractButtonPressed() {
-		return mechJoystick.getRawButton(RETRACT_ID);
+		return mechJoystick.getRawButton(RETRACT_BUTTON);
 	}
 
 	/**
@@ -94,7 +96,7 @@ public class TeleopInput {
 	 * @return True if button is pressed
 	 */
 	public boolean isShootHighButtonPressed() {
-		return mechJoystick.getRawButton(AIM_HIGH_ID);
+		return mechJoystick.getRawButton(AIM_HIGH_BUTTON);
 	}
 
 	/**
@@ -102,7 +104,7 @@ public class TeleopInput {
 	 * @return True if button is pressed
 	 */
 	public boolean isShootMidButtonPressed() {
-		return mechJoystick.getRawButton(AIM_MID_ID);
+		return mechJoystick.getRawButton(AIM_MID_BUTTON);
 	}
 
 	/* ------------------------ Right Joystick ------------------------ */
@@ -125,21 +127,21 @@ public class TeleopInput {
 	 * @return Axis value
 	 */
 	public boolean getCubeButton() {
-		return mechJoystick.getRawButton(1);
+		return mechJoystick.getRawButton(CUBE_BUTTON);
 	}
 	/**
 	 * Get Button to pick up cone.
 	 * @return Axis value
 	 */
 	public boolean getConeButton() {
-		return mechJoystick.getRawButton(2);
+		return mechJoystick.getRawButton(CONE_BUTTON);
 	}
 	/**
 	 * Get Button to release.
 	 * @return Axis value
 	 */
 	public boolean getOpenButton() {
-		return mechJoystick.getRawButton(RELEASE_BUTTON);
+		return mechJoystick.getRawButton(OPEN_BUTTON);
 	}
 	/* ======================== Private methods ======================== */
 
