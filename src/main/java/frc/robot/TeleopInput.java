e17a60b31f9794427a73cbcc75cd465978dbc6a9
Alex <92554149+alexyd88@users.noreply.github.com>
2023-01-16 17:36:25 -0800

    Resolved git comments

--- src/main/java/frc/robot/TeleopInput.java
+++ src/main/java/frc/robot/TeleopInput.java
@@ -100,7 +100,7 @@ public class TeleopInput {
 	 * Get Button to release.
 	 * @return Axis value
 	 */
-	public boolean getReleaseButton() {
+	public boolean getOpenButton() {
 		return mechJoystick.getRawButton(RELEASE_BUTTON);
 	}
 	/* ======================== Private methods ======================== */
