2b08ad885a5b69b8e78bd8d3d9b40c43078631aa
SamNat975 <sami.nat975@gmail.com>
2023-01-19 16:59:08 -0800

    attempted integration of pose and drive simulation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,48 +24,87 @@
 
  package frc.robot;
 
+ import edu.wpi.first.math.geometry.Pose3d;
  import edu.wpi.first.math.geometry.Rotation3d;
- import edu.wpi.first.math.geometry.Rotation2d;
  import edu.wpi.first.math.geometry.Transform3d;
  import edu.wpi.first.math.geometry.Translation3d;
+ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
  import edu.wpi.first.math.util.Units;
+ import org.photonvision.SimVisionTarget;
  
+ /**
+  * Holding class for all physical constants that must be used throughout the codebase. These values
+  * should be set by one of a few methods: 1) Talk to your mechanical and electrical teams and
+  * determine how the physical robot is being built and configured. 2) Read the game manual and look
+  * at the field drawings 3) Match with how your vision coprocessor is configured.
+  */
  public class Constants {
-     static class DriveTrainConstants {
-         static final double kMaxSpeed = 3.0; // meters per second
-         static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation per second
-         static final double kTrackWidth = 0.381 * 2; // meters
-         static final double kWheelRadius = 0.0508; // meters
-         static final int kEncoderResolution = 4096;
-         static final double distancePerPulse = 2 * Math.PI * kWheelRadius / (double) kEncoderResolution;
-     }
+     //////////////////////////////////////////////////////////////////
+     // Drivetrain Physical
+     //////////////////////////////////////////////////////////////////
+     public static final double kMaxSpeed = 3.0; // 3 meters per second.
+     public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second.
  
-     static class FieldConstants {
-         static final double length = Units.feetToMeters(54);
-         static final double width = Units.feetToMeters(27);
-     }
+     public static final double kTrackWidth = 0.381 * 2;
+     public static final double kWheelRadius = 0.0508;
+     public static final int kEncoderResolution = 4096;
  
-     static class VisionConstants {
-         static final Transform3d robotToCam =
-                 new Transform3d(
-                        // Cam mounted facing forward, half a meter forward of center, half a meter up
-                         new Translation3d(0.0, 0.0, 0.5), // x: forward/back, y: left/right, z: up/down
-                         new Rotation3d(
-                                 0, 0,
-                                 0)); 
-         // from center.
-         static final String cameraName = "gloworm";
-     }
-
-     static class AprilTagConstants {
-        static final double X1 = Units.inchesToMeters(610.77);
-        static final double Y1 = Units.inchesToMeters(42.19);
-        static final double Z1 = Units.inchesToMeters(18.22);
-        static final double rot1 = 180.0;
-
-        static final double X2 = Units.inchesToMeters(610.77);
-        static final double Y2 = Units.inchesToMeters(108.19);
-        static final double Z2 = Units.inchesToMeters(18.22);
-        static final double rot2 = 180.0;
-     }
+     public static final DifferentialDriveKinematics kDtKinematics =
+             new DifferentialDriveKinematics(kTrackWidth);
+ 
+     //////////////////////////////////////////////////////////////////
+     // Electrical IO
+     //////////////////////////////////////////////////////////////////
+     public static final int kGyroPin = 0;
+ 
+     public static final int kDtLeftEncoderPinA = 0;
+     public static final int kDtLeftEncoderPinB = 1;
+     public static final int kDtRightEncoderPinA = 2;
+     public static final int kDtRightEncoderPinB = 3;
+ 
+     public static final int kDtLeftLeaderPin = 1;
+     public static final int kDtLeftFollowerPin = 2;
+     public static final int kDtRightLeaderPin = 3;
+     public static final int kDtRightFollowerPin = 4;
+ 
+     //////////////////////////////////////////////////////////////////
+     // Vision Processing
+     //////////////////////////////////////////////////////////////////
+     // Name configured in the PhotonVision GUI for the camera
+     public static final String kCamName = "mainCam";
+ 
+     // Physical location of the camera on the robot, relative to the center of the
+     // robot.
+     public static final Transform3d kCameraToRobot =
+             new Transform3d(
+                     new Translation3d(-0.25, 0, -.25), // in meters
+                     new Rotation3d());
+ 
+     // See
+     // https://firstfrc.blob.core.windows.net/frc2020/PlayingField/2020FieldDrawing-SeasonSpecific.pdf
+     // page 208
+     public static final double targetWidth =
+             Units.inchesToMeters(41.30) - Units.inchesToMeters(6.70); // meters
+ 
+     // See
+     // https://firstfrc.blob.core.windows.net/frc2020/PlayingField/2020FieldDrawing-SeasonSpecific.pdf
+     // page 197
+     public static final double targetHeight =
+             Units.inchesToMeters(98.19) - Units.inchesToMeters(81.19); // meters
+ 
+     // See https://firstfrc.blob.core.windows.net/frc2020/PlayingField/LayoutandMarkingDiagram.pdf
+     // pages 4 and 5
+     public static final double kFarTgtXPos = Units.feetToMeters(54);
+     public static final double kFarTgtYPos =
+             Units.feetToMeters(27 / 2) - Units.inchesToMeters(43.75) - Units.inchesToMeters(48.0 / 2.0);
+     public static final double kFarTgtZPos =
+             (Units.inchesToMeters(98.19) - targetHeight) / 2 + targetHeight;
+ 
+     public static final Pose3d kFarTargetPose =
+             new Pose3d(
+                     new Translation3d(kFarTgtXPos, kFarTgtYPos, kFarTgtZPos),
+                     new Rotation3d(0.0, 0.0, Units.degreesToRadians(180)));
+ 
+     public static final SimVisionTarget kFarTarget =
+             new SimVisionTarget(kFarTargetPose, targetWidth, targetHeight, 42);
  }
