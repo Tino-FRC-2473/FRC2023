480b56fbcf56ae59feb36a87dd60121b5bedf4c6
Daiji Uchino <duchino292@student.fuhsd.org>
2023-01-20 16:59:58 -0800

    Made encoders reset only when necessary

--- src/main/java/frc/robot/systems/ArmFSM.java
+++ src/main/java/frc/robot/systems/ArmFSM.java
@@ -145,12 +145,8 @@ public class ArmFSM {
 					&& !input.isShootHighButtonPressed() && !input.isShootMidButtonPressed()) {
 					return FSMState.ARM_MOVEMENT;
 				} else if (input.isShootHighButtonPressed()) {
-					pivotMotor.getEncoder().setPosition(0);
-					teleArmMotor.getEncoder().setPosition(0);
 					return FSMState.SHOOT_HIGH;
 				} else if (input.isShootMidButtonPressed()) {
-					pivotMotor.getEncoder().setPosition(0);
-					teleArmMotor.getEncoder().setPosition(0);
 					return FSMState.SHOOT_MID;
 				}
 				return FSMState.IDLE;
@@ -158,8 +154,6 @@ public class ArmFSM {
 				if (input.isShootHighButtonPressed()) {
 					return FSMState.SHOOT_HIGH;
 				} else if (input.isShootMidButtonPressed()) {
-					pivotMotor.getEncoder().setPosition(0);
-					teleArmMotor.getEncoder().setPosition(0);
 					return FSMState.SHOOT_MID;
 				} else if (input.isExtendButtonPressed() || input.isRetractButtonPressed()
 					|| input.isPivotIncreaseButtonPressed()
