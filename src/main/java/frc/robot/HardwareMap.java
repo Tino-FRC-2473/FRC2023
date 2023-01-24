b1236dfd56624a9fbeae6ef96a2501e59ccac94e
Daiji Uchino <duchino292@student.fuhsd.org>
2023-01-23 17:18:32 -0800

    Updated HardwareMap to match main

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -8,8 +8,10 @@ import edu.wpi.first.wpilibj.DigitalInput;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-	public static final int CAN_ID_SPARK_PIVOT = 5;
-	public static final int CAN_ID_SPARK_TELEARM = 10;
+	public static final int CAN_ID_SPARK_PIVOT = 32;
+	public static final int CAN_ID_SPARK_TELEARM = 5;
+	public static final int CAN_ID_SPARK_DRIVE_RIGHT = 5;
+	public static final int CAN_ID_SPARK_DRIVE_LEFT = 6;
 
 
 	//grabber testing constants
