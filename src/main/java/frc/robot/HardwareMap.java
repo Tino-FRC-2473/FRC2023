242559fd0229fe3ca93327bae7fad643561b944d
Alex <92554149+alexyd88@users.noreply.github.com>
2023-02-03 18:49:13 -0800

    Created DistColorTester, which just gives color/dist, and updated distance sensor distances to more closely match hardware values

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -8,15 +8,15 @@ import edu.wpi.first.wpilibj.DigitalInput;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-	public static final int CAN_ID_SPARK_PIVOT = 10;
-	public static final int CAN_ID_SPARK_TELEARM = 5;
+	public static final int CAN_ID_SPARK_PIVOT = 5;
+	public static final int CAN_ID_SPARK_TELEARM = 10;
 	public static final int CAN_ID_SPARK_DRIVE_RIGHT = 5;
 	public static final int CAN_ID_SPARK_DRIVE_LEFT = 6;
 
 
 	//grabber testing constants
 	public static final int CAN_ID_SPINNER_MOTOR = 5;
-	public static final int ANALOGIO_ID_DISTANCE_SENSOR = 5;
+	public static final int ANALOGIO_ID_DISTANCE_SENSOR = 0;
 
 	// Place jumper from DIO pin 9 to GND to indicate this is a test setup
 	private static final int DIO_TEST_SETUP_CHANNEL = 9;
