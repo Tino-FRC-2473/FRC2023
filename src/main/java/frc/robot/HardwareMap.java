4e487b71305459083fa6625a4b9c1284a1abbddc
Daiji Uchino <103068628+duchino292@users.noreply.github.com>
2023-02-22 16:40:51 -0800

    Arm code with working fine-tuning code.
    Adjusted ARM_MOVEMENT state so it uses Joystick Y values for extension/retraction.
    Made a fine-tuning button which toggles the fine-tuning state. The inputs for extending/retracting and pivoting are the same but the motor powers are set to a small value.
    Made the code pivot and then extend if going from forward to backward or vice versa.

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-
 	public static final int CAN_ID_SPARK_DRIVE_RIGHT = 5;
 	public static final int CAN_ID_SPARK_DRIVE_LEFT = 6;
 
--- src/main/java/frc/robot/systems/DriveFSMSystem.java
+++ src/main/java/frc/robot/systems/DriveFSMSystem.java
@@ -216,6 +216,8 @@ public class DriveFSMSystem {
 
 			case P1N1:
 				moveState(input, true, Constants.P1X1, 0);
+				// set the grabber to be at the low state to drop off block
+				// reset the arm to idle state
 				break;
 
 			case P1N2:
@@ -230,6 +232,8 @@ public class DriveFSMSystem {
 
 			case P2N1:
 				moveState(input, true, Constants.P2X1, 0);
+				// set the grabber to be at the low state to drop off block
+				// reset the arm to idle state
 				break;
 
 			case P2N2:
@@ -240,14 +244,18 @@ public class DriveFSMSystem {
 
 			case P3N1:
 				moveState(input, true, Constants.P3X1, 0);
+				// set grabber at high height to drop off cube
 				break;
 
 			case P3N2:
 				moveState(input, false, Constants.P3X2, 0);
+				// set grabber at low height to pick up another cube
 				break;
 
 			case P3N3:
 				handleTurnState(input, Constants.P3A3);
+				// set grabnber at mid height to drop off cube
+				// reset the arm to idle state
 				break;
 
 			case P3N4:
